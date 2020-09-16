78442a7d6afb6e02111c190e0085f9880e3e8c6f
jasonmerc <jason.s.m@hotmail.com>
2020-09-16 19:41:05 -0400

    src/mainboard/dell/latitude_e7440: commit mainboard port using autoport script

---
+++ src/mainboard/dell/latitude_e7440/acpi/ec.asl
@@ -0,0 +1,7 @@
+Device(EC)
+{
+	Name (_HID, EISAID("PNP0C09"))
+	Name (_UID, 0)
+	Name (_GPE, 39)
+/* FIXME: EC support */
+}
---
+++ src/mainboard/dell/latitude_e7440/bootblock.c
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <southbridge/intel/lynxpoint/pch.h>
+
+void mainboard_config_superio(void)
+{
+}
