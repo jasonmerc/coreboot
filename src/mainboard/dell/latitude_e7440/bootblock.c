e4c6e3649771c1d356fec659ca965b24a6e26088
amersel <amersel@bibanon.org>
2020-10-04 22:19:41 -0400

    TBH i forget what this commit is but it probably has to do with enabling the EC

--- src/mainboard/dell/latitude_e7440/bootblock.c
+++ src/mainboard/dell/latitude_e7440/bootblock.c
@@ -1,7 +1,14 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
+#include <device/pci_ops.h>
 #include <southbridge/intel/lynxpoint/pch.h>
+#include <ec/dell/mec5055/mec5055.h>
 
 void mainboard_config_superio(void)
 {
+        /* port 0x910 and 0x911 is needed to initialize the EC */
+        pci_write_config32(PCH_LPC_DEV, LPC_GEN2_DEC, 0x007c0901);
+        mec5055_early_init();
+        pci_write_config32(PCH_LPC_DEV, LPC_GEN2_DEC, 0x005c0921);
+
 }
